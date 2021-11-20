d9793607e10ef5364da0169ea800c48fc86a1ce9
Eman <sayyedeman59@gmail.com>
2021-11-20 16:10:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Helo Emran</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, at.</p>
+    </div>
+  );
+}
+
+export default App;
